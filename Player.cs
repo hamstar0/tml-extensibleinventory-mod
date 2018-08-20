880f1e234788b363abcbe6b66f1a1d8fbc810807
hamstar0 <webutek@gmail.com>
2018-08-20 02:10:19 -0700

    v0.8.0 * Revised file layouts

--- Player.cs
+++ Player.cs
@@ -6,12 +6,6 @@ using Terraria.ModLoader.IO;
 
 namespace ExtensibleInventory {
 	partial class ExtensibleInventoryPlayer : ModPlayer {
-		public const int BasePageCapacity = 40;
-
-
-
-		////////////////
-
 		private readonly IList<Item[]> Pages = new List<Item[]>();
 		private int CurrentPage = 0;
 		private int MaxPage = 2;
@@ -75,61 +69,6 @@ namespace ExtensibleInventory {
 			}
 
 			return tags;
-		}
-
-
-		////////////////
-
-		private Item[] CreateBlankPage() {
-			var page = new Item[ExtensibleInventoryPlayer.BasePageCapacity];
-
-			for( int i = 0; i < ExtensibleInventoryPlayer.BasePageCapacity; i++ ) {
-				page[i] = new Item();
-			}
-
-			return page;
-		}
-
-		private void ResetPages() {
-			this.Pages.Clear();
-			this.Pages.Add( this.CreateBlankPage() );
-			this.Pages.Add( this.CreateBlankPage() );
-
-			this.MaxPage = this.Pages.Count;
-		}
-
-
-		////////////////
-
-		public void ScrollPageUp() {
-			if( this.CurrentPage <= 0 ) { return; }
-
-			this.DumpInventoryToPage( this.CurrentPage-- );
-			this.DumpPageToInventory( this.CurrentPage );
-		}
-
-		public void ScrollPageDown() {
-			if( this.CurrentPage >= (this.Pages.Count - 1) ) { return; }
-
-			this.DumpInventoryToPage( this.CurrentPage++ );
-			this.DumpPageToInventory( this.CurrentPage );
-		}
-
-
-		////////////////
-
-		private void DumpInventoryToPage( int page_num ) {
-			for( int i=10; i<50; i++ ) {
-				this.Pages[page_num][i - 10] = this.player.inventory[i];
-				this.player.inventory[i] = new Item();
-			}
-		}
-
-		private void DumpPageToInventory( int page_num ) {
-			for( int i=0; i<ExtensibleInventoryPlayer.BasePageCapacity; i++ ) {
-				this.player.inventory[i + 10] = this.Pages[page_num][i];
-				this.Pages[page_num][i] = new Item();
-			}
 		}
 	}
 }
---
+++ Player_Pages.cs
@@ -0,0 +1,65 @@
+using Terraria;
+using Terraria.ModLoader;
+
+
+namespace ExtensibleInventory {
+	partial class ExtensibleInventoryPlayer : ModPlayer {
+		public const int BasePageCapacity = 40;
+
+
+
+		////////////////
+
+		private Item[] CreateBlankPage() {
+			var page = new Item[ExtensibleInventoryPlayer.BasePageCapacity];
+
+			for( int i = 0; i < ExtensibleInventoryPlayer.BasePageCapacity; i++ ) {
+				page[i] = new Item();
+			}
+
+			return page;
+		}
+
+		private void ResetPages() {
+			this.Pages.Clear();
+			this.Pages.Add( this.CreateBlankPage() );
+			this.Pages.Add( this.CreateBlankPage() );
+
+			this.MaxPage = this.Pages.Count;
+		}
+
+
+		////////////////
+
+		public void ScrollPageUp() {
+			if( this.CurrentPage <= 0 ) { return; }
+
+			this.DumpInventoryToPage( this.CurrentPage-- );
+			this.DumpPageToInventory( this.CurrentPage );
+		}
+
+		public void ScrollPageDown() {
+			if( this.CurrentPage >= (this.Pages.Count - 1) ) { return; }
+
+			this.DumpInventoryToPage( this.CurrentPage++ );
+			this.DumpPageToInventory( this.CurrentPage );
+		}
+
+
+		////////////////
+
+		private void DumpInventoryToPage( int page_num ) {
+			for( int i=10; i<50; i++ ) {
+				this.Pages[page_num][i - 10] = this.player.inventory[i];
+				this.player.inventory[i] = new Item();
+			}
+		}
+
+		private void DumpPageToInventory( int page_num ) {
+			for( int i=0; i<ExtensibleInventoryPlayer.BasePageCapacity; i++ ) {
+				this.player.inventory[i + 10] = this.Pages[page_num][i];
+				this.Pages[page_num][i] = new Item();
+			}
+		}
+	}
+}
