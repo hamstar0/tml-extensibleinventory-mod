13b74b3631adf10dbb95f74da5a2537c051d45dc
hamstar0 <webutek@gmail.com>
2018-09-20 09:58:00 -0700

    v1.2.1.2 * Revised inventory dupe fix to solve issue of disappearing pages

--- Inventory/InventoryBook_UI.cs
+++ Inventory/InventoryBook_UI.cs
@@ -1,4 +1,5 @@
 using HamstarHelpers.Helpers.DebugHelpers;
+using HamstarHelpers.Helpers.TmlHelpers;
 using HamstarHelpers.Services.Timers;
 using Microsoft.Xna.Framework;
 using Terraria;
@@ -7,32 +8,27 @@ using Terraria;
 namespace ExtensibleInventory.Inventory {
 	partial class InventoryBook {
 		public bool ScrollPageUp( Player player ) {
+			if( !LoadHelpers.IsWorldSafelyBeingPlayed() ) {
+				return false;
+			}
 			if( Timers.GetTimerTickDuration("ExtensibleInventoryPageScroll") > 0 ) {
 				return false;
 			}
 			
 			string err;
-			int curr_page_idx = this.CurrentPageIdx;
 
 			if( !this.CanScrollPages( out err ) ) {
 				Main.NewText( err, Color.Red );
 				return false;
 			}
-			if( curr_page_idx <= 0 ) {
+			if( this.CurrentPageIdx <= 0 ) {
 				return false;
 			}
 			
-			this.DumpInventoryToPage( player, curr_page_idx );
+			this.DumpInventoryToPage( player, this.CurrentPageIdx );
+			this.DumpPageToInventory( player, --this.CurrentPageIdx );
 
 			Timers.SetTimer( "ExtensibleInventoryPageScroll", 10, () => {
-				if( !InventoryBook.IsPlayerInventoryEmpty( player ) ) {
-					this.DumpInventoryToPage( player, curr_page_idx );
-					return true;
-				}
-
-				this.CurrentPageIdx = curr_page_idx - 1;
-				this.DumpPageToInventory( player, this.CurrentPageIdx );
-
 				return false;
 			} );
 
@@ -40,32 +36,27 @@ namespace ExtensibleInventory.Inventory {
 		}
 
 		public bool ScrollPageDown( Player player ) {
+			if( !LoadHelpers.IsWorldSafelyBeingPlayed() ) {
+				return false;
+			}
 			if( Timers.GetTimerTickDuration("ExtensibleInventoryPageScroll") > 0 ) {
 				return false;
 			}
 			
 			string err;
-			int curr_page_idx = this.CurrentPageIdx;
 
 			if( !this.CanScrollPages( out err ) ) {
 				Main.NewText( err, Color.Red );
 				return false;
 			}
-			if( curr_page_idx >= ( this.Pages.Count - 1 ) ) {
+			if( this.CurrentPageIdx >= ( this.Pages.Count - 1 ) ) {
 				return false;
 			}
 
-			this.DumpInventoryToPage( player, curr_page_idx );
+			this.DumpInventoryToPage( player, this.CurrentPageIdx );
+			this.DumpPageToInventory( player, ++this.CurrentPageIdx );
 
 			Timers.SetTimer( "ExtensibleInventoryPageScroll", 10, () => {
-				if( !InventoryBook.IsPlayerInventoryEmpty( player ) ) {
-					this.DumpInventoryToPage( player, curr_page_idx );
-					return true;
-				}
-
-				this.CurrentPageIdx = curr_page_idx + 1;
-				this.DumpPageToInventory( player, this.CurrentPageIdx );
-
 				return false;
 			} );
 
